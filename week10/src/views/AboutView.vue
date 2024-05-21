092a6f2cd2b61bb996651e248f8099b458eeff0a
Umar Mukhtar <umaqgeek@gmail.com>
2024-05-21 22:02:43 +0800

    feat: add lab demo for week 10 with vue router

---
+++ week10/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <NavbarVue />
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarVue from "@/components/Navbar.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    NavbarVue,
+  },
+};
+</script>
